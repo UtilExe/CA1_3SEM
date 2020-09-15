8687cef994c71ffa67bd02393913e7e0bc54baac
jimmy <cph-jp327@cphbusiness.dk>
2020-09-15 14:32:44 +0200

    fetch from db javascript

---
+++ src/main/webapp/fetch.js
@@ -0,0 +1,29 @@
+let getAllMembersBtn = document.getElementById("getAllMembersBtn");
+getAllMembersBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    fetchAllMembers();
+});
+
+
+function fetchAllMembers() {
+    let url = 'https://work.emucoach.com/CA1/api/groupmembers/all';
+    let membersTable = document.getElementById("membersTable");
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            let newArray = data.map(x => `<tr><td>${x.name}</td><td>${x.studentID}</td><td>${x.favoriteTVSeries}</td></tr>`)
+            membersTable.innerHTML =
+                `<table class="table table-striped tableStyle">
+                    ${getTableHeader()}
+                    ${newArray.join("")}
+                </table>`
+        });
+}
+
+function getTableHeader(){
+    return `<thead><th>Name</th><th>Student ID</th><th>Favorite TV-series</th></thead>`;
+}
+
+function getTableRow(member){
+    return `<tr><td> </td><td> </td><td> </td></tr>`;
+}
